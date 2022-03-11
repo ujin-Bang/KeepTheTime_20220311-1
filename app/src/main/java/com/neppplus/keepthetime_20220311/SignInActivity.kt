1fd7185025998c7d58fc759c41979cb3f9122b10
nepp.plus <nepp.plus@gmail.com>
2022-03-11 16:51:23 +0900

    회원가입 화면 단순 추가 / 진입

--- app/src/main/java/com/neppplus/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/SignInActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.keepthetime_20220311
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -26,6 +27,13 @@ class SignInActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        binding.btnSignUp.setOnClickListener {
+
+            val myIntent = Intent(mContext, SignUpActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
         binding.btnLogin.setOnClickListener {
 
