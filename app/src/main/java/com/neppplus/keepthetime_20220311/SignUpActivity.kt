91cb7dac423f316c233ebcaf5f1dd2a893de4edf
nepp.plus <nepp.plus@gmail.com>
2022-03-11 16:56:44 +0900

    회원가입 클릭시 입력값 추출

--- app/src/main/java/com/neppplus/keepthetime_20220311/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/SignUpActivity.kt
@@ -18,6 +18,16 @@ class SignUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        binding.btnSignUp.setOnClickListener {
+
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+            val inputNickname = binding.edtNickname.text.toString()
+
+//            회원가입 API 호출 (PUT - "/user")
+
+        }
+
     }
 
     override fun setValues() {
