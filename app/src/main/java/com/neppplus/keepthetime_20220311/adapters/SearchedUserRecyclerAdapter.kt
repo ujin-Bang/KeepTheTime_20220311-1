a0f7eb7d9cc77d614adef759e5b03d8d86485999
nepp.plus <nepp.plus@gmail.com>
2022-03-14 16:08:45 +0900

    리싸이클러뷰의 어댑터 제작 중  - 필수 오버라이딩 함수 3개 생성  - 몇줄짜리 리싸이클러뷰인지? getItemCount  - 어댑터의 생성자로 Context / List를 받아서 활용.

--- app/src/main/java/com/neppplus/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -1,12 +1,29 @@
 package com.neppplus.keepthetime_20220311.adapters
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.keepthetime_20220311.datas.UserData
 
-class SearchedUserRecyclerAdapter : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
+class SearchedUserRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<UserData>
+) : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+//    몇개의 아이템을 보여줄 예정인지? => 데이터목록의 갯수만큼.
+    override fun getItemCount() = mList.size
 
 }
