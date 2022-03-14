3dbb7414fb2c6a02c788caaa64c2052d76ed664b
nepp.plus <nepp.plus@gmail.com>
2022-03-14 14:44:35 +0900

    리스트뷰 어댑터 뼈대작업 마무리

--- app/src/main/java/com/neppplus/keepthetime_20220311/adapters/MyFriendAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/adapters/MyFriendAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.keepthetime_20220311.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.keepthetime_20220311.R
 import com.neppplus.keepthetime_20220311.datas.UserData
 
 class MyFriendAdapter(
@@ -10,6 +14,17 @@ class MyFriendAdapter(
     val mList: List<UserData>
 ) : ArrayAdapter<UserData>(mContext, resId, mList) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow =  LayoutInflater.from(mContext).inflate(R.layout.my_friend_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
 
 }
