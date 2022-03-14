5723c111dfe4d25711886d74148204ca3dc5581c
nepp.plus <nepp.plus@gmail.com>
2022-03-14 12:18:50 +0900

    친구 목록 화면 진입

--- app/src/main/java/com/neppplus/keepthetime_20220311/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/fragments/MyProfileFragment.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.neppplus.keepthetime_20220311.ManageMyFriendsActivity
 import com.neppplus.keepthetime_20220311.R
 import com.neppplus.keepthetime_20220311.SplashActivity
 import com.neppplus.keepthetime_20220311.databinding.FragmentMyProfileBinding
@@ -40,6 +41,13 @@ class MyProfileFragment : BaseFragment() {
     }
 
     override fun setupEvents() {
+
+        binding.btnManageMyFriends.setOnClickListener {
+
+            val myIntent = Intent(mContext, ManageMyFriendsActivity::class.java)
+            startActivity(myIntent)
+
+        }
 
         binding.btnLogout.setOnClickListener {
 
