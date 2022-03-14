c77f9d89db3d2006956f958e1835d87653adf6c6
nepp.plus <nepp.plus@gmail.com>
2022-03-14 15:11:44 +0900

    친구 목록 실제 데이터 반영

--- app/src/main/java/com/neppplus/keepthetime_20220311/adapters/MyFriendAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/adapters/MyFriendAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.neppplus.keepthetime_20220311.R
 import com.neppplus.keepthetime_20220311.datas.UserData
 
@@ -23,6 +26,16 @@ class MyFriendAdapter(
 
         val row = tempRow!!
 
+        val data = mList[position]
+
+        val imgProfile = row.findViewById<ImageView>(R.id.imgProfile)
+        val txtNickname = row.findViewById<TextView>(R.id.txtNickname)
+        val txtEmail = row.findViewById<TextView>(R.id.txtEmail)
+
+
+        Glide.with(mContext).load(data.profile_img).into(imgProfile)
+        txtNickname.text = data.nick_name
+        txtEmail.text = data.email
 
         return row
     }
