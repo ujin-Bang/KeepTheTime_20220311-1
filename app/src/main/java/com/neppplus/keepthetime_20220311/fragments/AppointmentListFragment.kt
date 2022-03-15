a7677c9bf102c00274d7003678437371cf5208d6
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-15 14:58:16 +0900

    약속 등록 화면 추가

--- app/src/main/java/com/neppplus/keepthetime_20220311/fragments/AppointmentListFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/fragments/AppointmentListFragment.kt
@@ -1,11 +1,13 @@
 package com.neppplus.keepthetime_20220311.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.neppplus.keepthetime_20220311.EditAppointmentActivity
 import com.neppplus.keepthetime_20220311.R
 import com.neppplus.keepthetime_20220311.databinding.FragmentAppointmentListBinding
 
@@ -29,12 +31,18 @@ class AppointmentListFragment : BaseFragment() {
     }
 
     override fun setupEvents() {
+
+        binding.btnAddAppointment.setOnClickListener {
+
+        val myIntent = Intent(mContext, EditAppointmentActivity::class.java)
+            startActivity(myIntent)
+        }
         
     }
 
     override fun setValues() {
         
-        binding.txtTest.text = "코틀린에서 문구 바꾸기"
+
         
     }
 
