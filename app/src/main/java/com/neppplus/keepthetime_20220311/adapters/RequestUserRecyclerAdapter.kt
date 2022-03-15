cccf9a0fd7ce8fef88a1aabf8c39988484a0f97c
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-15 11:44:50 +0900

    두개의 버튼이 눌리면 할 일을 하나의 코드로 표현  - tag속성에 각 버튼의 메모를 남겨서 활용

--- app/src/main/java/com/neppplus/keepthetime_20220311/adapters/RequestUserRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/adapters/RequestUserRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.neppplus.keepthetime_20220311.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +24,8 @@ class RequestUserRecyclerAdapter(
         val txtNickname = view.findViewById<TextView>(R.id.txtNickname)
         val imgSocialLoginLogo = view.findViewById<ImageView>(R.id.imgSocialLoginLogo)
         val txtEmail = view.findViewById<TextView>(R.id.txtEmail)
+        val btnAccept = view.findViewById<Button>(R.id.btnAccept)
+        val btnDeny = view.findViewById<Button>(R.id.btnDeny)
 
         fun bind(data: UserData) {
 
@@ -60,6 +64,27 @@ class RequestUserRecyclerAdapter(
 //                그 외의 잘못된 경우.
                 }
             }
+
+            // btnAccept, btnDeny 모두 같은 API를 호출( 하는 행동이 같다)
+//            => type 파라미터에 첨부하는 값만 다름( "수락"/"거절")
+
+//            두개의 버튼이 눌리면 할 일을 하나의 변수에 담아두자.(같은 일을 하게)
+            //할 일: Interface => 정석 ->  object:인터페이스 종류{ }
+//            축약문법(lamda) => 인터페이스 종류{  }
+            val ocl = View.OnClickListener {
+
+//                서버에 수락 / 거절 의사 전달.
+//                수락버튼눌려야 :수락 / 거절버튼 눌려야: 거절 => 어느 버튼이 눌렸는지? 파악 가능해야 파라미터도 다르게 전달.
+//                it 변수: 클릭된 버튼을 담고 있는 역할.
+//                tag 속성: 아무 말이나 적어도 되는 일종의 메모. 수락/ 거절 등 보내야 할 값을 메모해두자.
+
+                val tagStr = it.tag.toString()
+
+                Log.d("보낼 파라미터값",tagStr)
+            }
+
+            btnAccept.setOnClickListener(ocl)
+            btnDeny.setOnClickListener(ocl)
 
         }
 
