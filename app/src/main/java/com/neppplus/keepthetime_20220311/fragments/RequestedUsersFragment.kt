9fb560fbc5f3c0c273b4f5ddb68dea542f9d3f53
nepp.plus <nepp.plus@gmail.com>
2022-03-15 11:18:18 +0900

    친구 요청 사용자 목록도 리싸이클러뷰로 표시  - Adapter의 구조가, 복사/붙여넣기 해서 사용해도 될 정도로 정형화되어있다.

--- app/src/main/java/com/neppplus/keepthetime_20220311/fragments/RequestedUsersFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/fragments/RequestedUsersFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.neppplus.keepthetime_20220311.R
+import com.neppplus.keepthetime_20220311.adapters.RequestUserRecyclerAdapter
 import com.neppplus.keepthetime_20220311.databinding.FragmentMyFriendsBinding
 import com.neppplus.keepthetime_20220311.databinding.FragmentRequestedUsersBinding
+import com.neppplus.keepthetime_20220311.datas.BasicResponse
+import com.neppplus.keepthetime_20220311.datas.UserData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RequestedUsersFragment : BaseFragment() {
 
     lateinit var binding: FragmentRequestedUsersBinding
+
+    val mRequestedUserList = ArrayList<UserData>()
+
+    lateinit var mAdapter: RequestUserRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,5 +45,42 @@ class RequestedUsersFragment : BaseFragment() {
 
     override fun setValues() {
 
+//        나에게 친구 요청한 사람 목록을 > 리싸이클러뷰로 보여주기.
+//        API : getRequestFriendList 함수 -> "requested" 로 대입.
+
+        getRequestedUsersFromServer()
+
+
+        mAdapter = RequestUserRecyclerAdapter(mContext, mRequestedUserList)
+        binding.requestUsersRecyclerView.adapter = mAdapter
+
+        binding.requestUsersRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
     }
+
+    fun getRequestedUsersFromServer() {
+
+        apiList.getRequestFriendList("requested").enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if (response.isSuccessful) {
+
+                    mRequestedUserList.clear()
+
+                    mRequestedUserList.addAll( response.body()!!.data.friends )
+
+                    mAdapter.notifyDataSetChanged()
+
+                }
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
+    }
+
 }
