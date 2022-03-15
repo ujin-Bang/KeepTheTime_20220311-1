fd118b540415ae796c04ba0ea0995af341b79b86
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-15 12:22:41 +0900

    뷰페이저2의 어댑터 클래스 제작 > 뷰페이저2와 연결

--- app/src/main/java/com/neppplus/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/MainActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.keepthetime_20220311
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_20220311.adapters.MainViewPager2Adapter
 import com.neppplus.keepthetime_20220311.adapters.MainViewPagerAdapter
 import com.neppplus.keepthetime_20220311.databinding.ActivityMainBinding
 import com.neppplus.keepthetime_20220311.datas.BasicResponse
@@ -29,6 +30,7 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+        binding.mainViewPager2.adapter = MainViewPager2Adapter(this) // 변수: Activity =>객체:Context로 대입 불가.
 
     }
 }
