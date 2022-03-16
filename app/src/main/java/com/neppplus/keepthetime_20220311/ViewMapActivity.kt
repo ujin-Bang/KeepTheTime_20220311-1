cc27b048e943d4ccbf4ef07a30d81170c39a1d8a
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-16 16:11:10 +0900

    지도 띄우기 + 카메라 이동 + 마커설정

--- app/src/main/java/com/neppplus/keepthetime_20220311/ViewMapActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/ViewMapActivity.kt
@@ -3,6 +3,9 @@ package com.neppplus.keepthetime_20220311
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.overlay.Marker
 import com.neppplus.keepthetime_20220311.databinding.ActivityViewMapBinding
 import com.neppplus.keepthetime_20220311.datas.AppointmentData
 
@@ -25,6 +28,23 @@ class ViewMapActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        binding.naverMapView.getMapAsync {
+
+            val naverMap = it
+//            도착지로 카메라 이동
+
+//            도착지 자체를 변수화
+            val destLatLng =  LatLng(mAppointment.latitude, mAppointment.longitude)
+
+            val cameraUpdate = CameraUpdate.scrollTo(destLatLng)
+            naverMap.moveCamera(cameraUpdate)
+
+//            마커도 찍어주자.
+            val marker = Marker()
+            marker.position = destLatLng
+            marker.map = naverMap
+        }
 
     }
 }
