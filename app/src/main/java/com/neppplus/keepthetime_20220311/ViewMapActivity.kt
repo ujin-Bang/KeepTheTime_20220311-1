152c0e22cfbcc38aa50e689756b173c6f8635008
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-16 16:04:05 +0900

    약속 데이터를 첨부해서 지도 화면으로 이동

--- app/src/main/java/com/neppplus/keepthetime_20220311/ViewMapActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/ViewMapActivity.kt
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.keepthetime_20220311.databinding.ActivityViewMapBinding
+import com.neppplus.keepthetime_20220311.datas.AppointmentData
 
 class ViewMapActivity : BaseActivity() {
 
     lateinit var binding: ActivityViewMapBinding
 
+    lateinit var mAppointment: AppointmentData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_view_map)
+        mAppointment = intent.getSerializableExtra("appointment") as AppointmentData
         setupEvents()
         setValues()
     }
