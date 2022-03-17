d6697fe8fe0cb7c2c02affd809b8b5960d6e8c4c
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-17 11:47:08 +0900

    내 장소 목록을 서버에서 받아서 ArrayList에 담아주기

--- app/src/main/java/com/neppplus/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220311/EditAppointmentActivity.kt
@@ -16,6 +16,7 @@ import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.PathOverlay
 import com.neppplus.keepthetime_20220311.databinding.ActivityEditAppointmentBinding
 import com.neppplus.keepthetime_20220311.datas.BasicResponse
+import com.neppplus.keepthetime_20220311.datas.PlaceData
 import com.odsay.odsayandroidsdk.API
 import com.odsay.odsayandroidsdk.ODsayData
 import com.odsay.odsayandroidsdk.ODsayService
@@ -39,6 +40,9 @@ class EditAppointmentActivity : BaseActivity() {
     var path: PathOverlay? = null //출발지 ~도착지까지 보여줄 경로선, 처음에는 보이지 앟는 상태
 
     var mSelectedLatLng : LatLng? = null //약속 장소 위/경도 처음에는 설정하지 않은 상태
+
+//    내 출발장소 목록
+    val mStartPlaceList = ArrayList<PlaceData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -374,6 +378,31 @@ class EditAppointmentActivity : BaseActivity() {
             }
 
         }
+
+//        내 출발장소 목록 불러오기
+        getMyStartPlaceListFromServer()
+
+    }
+    fun getMyStartPlaceListFromServer(){
+
+        apiList.getRequestMyPlaceList().enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                if(response.isSuccessful){
+
+                    val br = response.body()!!
+
+                    mStartPlaceList.clear()
+
+                    mStartPlaceList.addAll(br.data.places)
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
 
     }
 }
