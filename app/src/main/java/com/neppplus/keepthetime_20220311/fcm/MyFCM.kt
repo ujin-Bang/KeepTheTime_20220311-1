615d9c237a515ce758952ca774e77d3f7b5c226c
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-03-21 10:49:01 +0900

    앱이 켜져있을 때 알림이 오면 처리할 서비스 클래스 추가  - data-message형태가 오면 해당 서비스에서 알림 울리는 코드 추가 작성

---
+++ app/src/main/java/com/neppplus/keepthetime_20220311/fcm/MyFCM.kt
@@ -0,0 +1,28 @@
+package com.neppplus.keepthetime_20220311.fcm
+
+import android.os.Looper
+import android.widget.Toast
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import java.util.logging.Handler
+
+class MyFCM: FirebaseMessagingService() {
+
+//    메세지 수신시 할 일.
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+
+//        예시 : notification이 왔을 때.
+//        => 토스트로 알림의 제목만 출력.
+
+        val title = p0.notification!!.title!!
+
+//        UI쓰레드에게 토스트 띄우는 업무 부여.
+        val myHandler = Handler(Looper.getMainLooper())
+          myHandler.post{
+            Toast.makeText(applicationContext, title, Toast.LENGTH_SHORT).show()
+        }
+        Toast.makeText(applicationContext, title, Toast.LENGTH_SHORT).show()
+    }
+}
